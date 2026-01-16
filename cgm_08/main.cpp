5778026f790acfc12ae42324bd4197160cbe4349
Tobias Reimann <info@tobias-reimann.com>
2026-01-16 07:50:29 +0100

    Removed some comments and empty lines

--- cgm_08/main.cpp
+++ cgm_08/main.cpp
@@ -47,15 +47,13 @@ int main()
 
         double deltaTime = 0.0;
 
-        // The main loop consists of 3 ordered components: window -> simulation -> renderer.
-        // The window loop handles keyboard/mouse input and records time passed between frames.
         while (window.loop(deltaTime))
         {
-            // simulation loop handles gameplay, e.g. moves entities and camera by an internal logic.
             simulation.loop(deltaTime);
+            
             renderer.setViewMatrix(simulation.getCameraViewMatrix());
             renderer.setCameraPosition(simulation.getCameraPosition());
-            // The renderer loop draws entities seen through the camera with the given matrices.
+
             renderer.loop();
         }
     }
--- cgm_09/filereader.h
+++ cgm_09/filereader.h
@@ -18,6 +18,7 @@
  */
 
 #pragma once
+
 #include "cgmath.h"
 
 #include <fstream>
